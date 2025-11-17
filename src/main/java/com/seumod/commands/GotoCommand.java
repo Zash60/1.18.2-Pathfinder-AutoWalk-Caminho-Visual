536e99f6a116577b5a0d44e7a9bfa198b1318c4f
Zash60 <xath06@gmail.com>
2025-11-17 13:00:05 -0300

    Update GotoCommand.java

--- src/main/java/com/seumod/commands/GotoCommand.java
+++ src/main/java/com/seumod/commands/GotoCommand.java
@@ -2,15 +2,17 @@ package com.seumod.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.seumod.pathfinder.PathfinderManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.LiteralText;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+
 import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class GotoCommand {
     public static void register() {
-        net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("goto")
                 .then(argument("x", IntegerArgumentType.integer())
                     .then(argument("y", IntegerArgumentType.integer())
@@ -19,10 +21,11 @@ public class GotoCommand {
                                 int x = getInteger(context, "x");
                                 int y = getInteger(context, "y");
                                 int z = getInteger(context, "z");
-                                
-                                PathfinderManager.setTarget(x, y, z);
+
+                                // ANOTAÇÃO: Usamos o objeto Text moderno em vez de LiteralText
+                                PathfinderManager.setTarget(new BlockPos(x, y, z));
                                 context.getSource().sendFeedback(
-                                    new LiteralText("§aDestino: [" + x + ", " + y + ", " + z + "]"), 
+                                    Text.literal("§aCalculando caminho para: [" + x + ", " + y + ", " + z + "]"),
                                     false
                                 );
                                 return 1;
@@ -30,6 +33,15 @@ public class GotoCommand {
                         )
                     )
                 )
+            );
+
+            // ANOTAÇÃO: Adicionado um comando /stop para conveniência.
+            dispatcher.register(literal("stop")
+                .executes(context -> {
+                    PathfinderManager.setEnabled(false);
+                    context.getSource().sendFeedback(Text.literal("§cNavegação interrompida."), false);
+                    return 1;
+                })
             );
         });
     }
