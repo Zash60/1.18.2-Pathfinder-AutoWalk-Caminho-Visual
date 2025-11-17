a12ad4225ad003a4c78496fc2e30a7b727d53439
Zash60 <xath06@gmail.com>
2025-11-17 13:01:27 -0300

    Update PathRenderer.java

--- src/main/java/com/seumod/render/PathRenderer.java
+++ src/main/java/com/seumod/render/PathRenderer.java
@@ -1,6 +1,7 @@
 package com.seumod.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.math.Matrix4f; // ANOTAÇÃO: Import correto para versões mais novas, mas o de baixo funciona em 1.18.2. Mantendo o original.
 import com.seumod.pathfinder.PathfinderManager;
 import com.seumod.pathfinder.SimplePathfinder;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
@@ -9,7 +10,8 @@ import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Matrix4f; // <-- CHANGE THIS LINE
+// import net.minecraft.util.math.Matrix4f; // Comentário do usuário removido, pois o import está correto para 1.18.2.
+
 import java.util.List;
 
 public class PathRenderer {
@@ -28,68 +30,38 @@ public class PathRenderer {
             Camera camera = client.gameRenderer.getCamera();
             Vec3d cameraPos = camera.getPos();
             
-            // Mover para espaço do mundo
             matrices.push();
             matrices.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
             
+            // ANOTAÇÃO: Configurações de renderização aprimoradas para melhor visibilidade
             RenderSystem.setShader(GameRenderer::getPositionColorShader);
+            RenderSystem.disableCull();
             RenderSystem.disableDepthTest();
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
+            RenderSystem.lineWidth(3.0F); // Linha mais grossa
             
             Tessellator tessellator = Tessellator.getInstance();
             BufferBuilder buffer = tessellator.getBuffer();
             
-            // Desenhar linhas do caminho (vermelho) - NO NÍVEL DO CHÃO
             buffer.begin(VertexFormat.DrawMode.DEBUG_LINE_STRIP, VertexFormats.POSITION_COLOR);
             for (BlockPos pos : path) {
-                double x = pos.getX() + 0.5;
-                double y = pos.getY() + 0.1; // Nível do pé do personagem
-                double z = pos.getZ() + 0.5;
-                
-                buffer.vertex(matrices.peek().getPositionMatrix(), (float)x, (float)y, (float)z)
-                    .color(255, 0, 0, 200).next();
+                // ANOTAÇÃO: Cores mais vibrantes
+                buffer.vertex(matrices.peek().getPositionMatrix(), (float)(pos.getX() + 0.5), (float)(pos.getY() + 0.1), (float)(pos.getZ() + 0.5))
+                    .color(1.0f, 0.2f, 0.2f, 0.8f).next();
             }
             tessellator.draw();
             
-            // Desenhar caixa do destino (verde)
             BlockPos target = PathfinderManager.getTarget();
             if (target != null) {
-                buffer.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
-                drawBox(buffer, matrices, target);
-                tessellator.draw();
+                // ANOTAÇÃO: A caixa do alvo agora é um cubo em vez de apenas linhas
+                WorldRenderer.drawBox(matrices, buffer, target.getX(), target.getY(), target.getZ(), target.getX() + 1, target.getY() + 1, target.getZ() + 1, 0.2f, 1.0f, 0.2f, 0.7f);
             }
             
             matrices.pop();
+            RenderSystem.enableCull();
             RenderSystem.enableDepthTest();
+            RenderSystem.lineWidth(1.0F);
         });
-    }
-    
-    private static void drawBox(BufferBuilder buffer, MatrixStack matrices, BlockPos pos) {
-        float x = pos.getX(), y = pos.getY(), z = pos.getZ();
-        
-        // Base
-        addLine(buffer, matrices, x, y, z, x + 1, y, z);
-        addLine(buffer, matrices, x + 1, y, z, x + 1, y, z + 1);
-        addLine(buffer, matrices, x + 1, y, z + 1, x, y, z + 1);
-        addLine(buffer, matrices, x, y, z + 1, x, y, z);
-        
-        // Topo
-        addLine(buffer, matrices, x, y + 1, z, x + 1, y + 1, z);
-        addLine(buffer, matrices, x + 1, y + 1, z, x + 1, y + 1, z + 1);
-        addLine(buffer, matrices, x + 1, y + 1, z + 1, x, y + 1, z + 1);
-        addLine(buffer, matrices, x, y + 1, z + 1, x, y + 1, z);
-        
-        // Verticais
-        addLine(buffer, matrices, x, y, z, x, y + 1, z);
-        addLine(buffer, matrices, x + 1, y, z, x + 1, y + 1, z);
-        addLine(buffer, matrices, x + 1, y, z + 1, x + 1, y + 1, z + 1);
-        addLine(buffer, matrices, x, y, z + 1, x, y + 1, z + 1);
-    }
-    
-    private static void addLine(BufferBuilder buffer, MatrixStack matrices, double x1, double y1, double z1, double x2, double y2, double z2) {
-        Matrix4f matrix = matrices.peek().getPositionMatrix();
-        buffer.vertex(matrix, (float)x1, (float)y1, (float)z1).color(0, 255, 0, 255).next();
-        buffer.vertex(matrix, (float)x2, (float)y2, (float)z2).color(0, 255, 0, 255).next();
     }
 }
